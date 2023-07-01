24702b3b54febd59544856b8903246dc2341f655
Sara Paguaga <64711979+MGonza20@users.noreply.github.com>
2023-06-30 22:47:42 -0600

    show error if empty fields

--- frontend/src/components/AddEmployeeDialog.jsx
+++ frontend/src/components/AddEmployeeDialog.jsx
@@ -1,4 +1,3 @@
-
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -9,93 +8,81 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 export default function AddEmployeeDialog({ open, handleClose }) {
-  
+  const [employee, setEmployee] = React.useState({
+    'Código': '',
+    'Nombre': '',
+    'Apellido': '',
+    'Dirección': '',
+    'Puesto': '',
+    'Correo Electrónico': '',
+    'Teléfono': '',
+    'Teléfono Secundario': ''
+  });
+
+  const [first, setFirst] = React.useState(true);
+
+  const handleInputChange = (event) => {
+    setEmployee({
+      ...employee,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const isFormEmpty = () => {
+
+    let fields = { ...employee };
+    delete fields['Teléfono Secundario'];
+
+    for (let key in fields) {
+      if (fields[key] === '') {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const handleAccept = () => {
+    if (first) {
+      setFirst(false);
+      return;
+    }
+
+    if (!isFormEmpty() && !first) {
+      handleClose();
+    }
+  };
+
   return (
     <div>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle>Agregar información de empleado</DialogTitle>
         <DialogContent>
           <DialogContentText>
-          Para añadir datos de un empleado, por favor proporcione 
-          la información en los campos siguientes.
+            Para añadir datos de un empleado, por favor proporcione 
+            la información en los campos siguientes.
           </DialogContentText>
 
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Código"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Nombre"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Apellido"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Dirección"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Puesto"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Correo Electrónico"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Teléfono"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
-          <TextField
-            autoFocus
-            margin="dense"
-            label="Teléfono Secundario"
-            type="text"
-            fullWidth
-            variant="standard"
-          />
-
+          {Object.keys(employee).map((key, index) => (
+            <TextField
+              key={index}
+              autoFocus
+              margin="dense"
+              label={key}
+              type="text"
+              fullWidth
+              variant="standard"
+              required={key !== 'Teléfono Secundario'}
+              error={key !== 'Teléfono Secundario' && employee[key] === '' && !first}
+              helperText={key !== 'Teléfono Secundario' &&  employee[key] === '' && !first ? 'Campo requerido.' : ''}
+              name={key}
+              value={employee[key]}
+              onChange={handleInputChange}
+            />
+          ))}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleClose}>Aceptar</Button>
+          <Button onClick={handleAccept}>Aceptar</Button>
         </DialogActions>
       </Dialog>
     </div>
