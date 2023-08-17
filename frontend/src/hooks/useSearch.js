7ed2ac393557eaf8ccc2abd17dbb3dc60277ba8b
Guillermo Santos <guillermo.santosba@gmail.com>
2023-08-17 10:48:43 -0600

    replace ip

--- frontend/src/hooks/useSearch.js
+++ frontend/src/hooks/useSearch.js
@@ -2,20 +2,23 @@ import { useState, useEffect } from "react";
 import { getQuery } from '../helpers/search';
 
 export const useSearch = (query) => {
-
+	
     const [searchRes, setSearchRes] = useState([]);
     const [isLoadingSearch, setLoadingSearch] = useState(true);
 
 
-    const geSearchData = async () => {
+	
+
+	useEffect(() => {
+			
+	const geSearchData = async () => {
+			console.log('searching')
         const queryResult = await getQuery(query);
         setSearchRes(queryResult);
         setLoadingSearch(false);
-    }
-
-    useEffect(() => {
+	}
         geSearchData();
-    });  
+    },[query]);  
     
     return {
         searchRes,
