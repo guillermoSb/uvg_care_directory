77cc68de750670dfc4c349ddb17978cf0ae32724
Guillermo Santos <guillermo.santosba@gmail.com>
2023-11-21 01:24:25 -0600

    possible fix

--- frontend/src/hooks/useFetchEmployees.js
+++ frontend/src/hooks/useFetchEmployees.js
@@ -1,23 +1,28 @@
-import { useState, useEffect } from "react";
-import { getEmployees } from "../helpers/getEmployees";
+import { useState, useEffect } from 'react';
+import { getEmployees } from 'your-api-utils'; // Replace with your actual API utility function
 
 export const useFetchEmployees = () => {
-
     const [employees, setEmployees] = useState([]);
     const [isLoading, setLoading] = useState(true);
 
     const getEmployeesData = async () => {
-        const employeeData = await getEmployees();
-        setEmployees(employeeData);
-        setLoading(false);
-    }
+        try {
+            const employeeData = await getEmployees();
+            setEmployees(employeeData);
+            setLoading(false);
+        } catch (error) {
+            // Handle errors appropriately, e.g., set an error state
+            console.error('Error fetching employees:', error);
+            setLoading(false);
+        }
+    };
 
     useEffect(() => {
         getEmployeesData();
-    }, [employees]);  
-    
+    }, []);  // Empty dependency array to run the effect only once on mount
+
     return {
         employees,
-        isLoading
-    }
-}
+        isLoading,
+    };
+};
