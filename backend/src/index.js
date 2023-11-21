e5a11729751760b1718e187187492be7ae088c1b
Guillermo Santos <guillermo.santosba@gmail.com>
2023-11-21 00:55:06 -0600

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,12 +9,19 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
+app.get('/api/employees', fetchAllEmployees);
 app.get('/employees', fetchAllEmployees);
 app.get('/employees/search', searchEmployee);
+app.get('/api/employees/search', searchEmployee);
+
 app.post('/employees', addEmployee);
+app.post('/api/employees', addEmployee);
 app.put('/employees/:employeeCode', updateEmployee);
+app.put('/api/employees/:employeeCode', updateEmployee);
 app.delete('/employees/:employeeCode', removeEmployee);
+app.delete('/api/employees/:employeeCode', removeEmployee);
 app.delete('/employees', deleteAllEmployees);
+app.delete('/api/employees', deleteAllEmployees);
 
 // Start Listening
 app.listen(process.env.PORT || 3000, () => {
